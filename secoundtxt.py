aea99646754c04f67f2e7c1572f9ec3ac9e02ce4
sriramkalidindi8-rgb <sriramkalidindi8@gmail.com>
2026-09-24 12:13:49 +0530

    Add files via upload

---
+++ secoundtxt.py
@@ -0,0 +1,9 @@
+Python 3.9.7 (tags/v3.9.7:1016ef3, Aug 30 2021, 20:19:38) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> print(1100+12102)
+13202
+>>> print(3728928+18829)
+3747757
+>>> print("hi hello")
+hi hello
+>>> 
